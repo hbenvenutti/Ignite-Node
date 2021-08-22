94c3cdbd271484293a958e12a94b88f8f5f5de2d
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-08-21 22:24:15 -0300

    supertest create category

--- src/modules/cars/use-cases/category/list-categories/__tests__/ListCategories.controller.spec.ts
+++ src/modules/cars/use-cases/category/list-categories/__tests__/ListCategories.controller.spec.ts
@@ -8,7 +8,7 @@ import createConnection from '@shared/infra/typeorm';
 
 let connection: Connection;
 
-describe('Create Category Controller', () => {
+describe('List Category Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -21,43 +21,28 @@ describe('Create Category Controller', () => {
       `
     );
   });
-
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close();
   });
 
-  it('should create a new category', async () => {
+  it('should list all categories', async () => {
     const session = await request(app)
       .post('/sessions')
       .send({ email: 'admin@rentx.com', password: 'admin' });
 
     const { token } = session.body;
 
-    const response = await request(app)
-      .post('/categories')
-      .send({ name: 'Supertest', description: 'supertest' })
-      .set({ Authorization: `Bearer ${token}` });
-
-    expect(response.status).toBe(201);
-  });
-
-  it('should not create a duplicate category', async () => {
-    const session = await request(app)
-      .post('/sessions')
-      .send({ email: 'admin@rentx.com', password: 'admin' });
-
-    const { token } = session.body;
     await request(app)
       .post('/categories')
       .send({ name: 'Supertest', description: 'supertest' })
       .set({ Authorization: `Bearer ${token}` });
 
-    const response = await request(app)
-      .post('/categories')
-      .send({ name: 'Supertest', description: 'supertest' })
-      .set({ Authorization: `Bearer ${token}` });
+    const response = await request(app).get('/categories');
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0].name).toEqual('Supertest');
   });
 });
